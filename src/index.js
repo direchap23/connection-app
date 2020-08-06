9c2388a54d868e6caf7a3e53a975e94e1e29ee66
Meliodas <redwin1823@gmail.com>
2020-08-07 03:02:57 +0800

    modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// These must be the first lines in src/index.js
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+// End here for IE11
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './tailwind.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
